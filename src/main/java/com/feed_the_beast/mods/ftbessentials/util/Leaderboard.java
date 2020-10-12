caaee9ffe1639b2b5fe9e2d8ab8e496792bd778a
LatvianModder <latvianmodder@gmail.com>
2020-10-12 22:32:29 +0300

    Fixed crash on server

--- src/main/java/com/feed_the_beast/mods/ftbessentials/util/Leaderboard.java
+++ src/main/java/com/feed_the_beast/mods/ftbessentials/util/Leaderboard.java
@@ -1,10 +1,14 @@
 package com.feed_the_beast.mods.ftbessentials.util;
 
-import net.minecraft.stats.IStatFormatter;
 import net.minecraft.stats.ServerStatisticsManager;
 import net.minecraft.stats.Stats;
+import net.minecraft.util.Util;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -14,6 +18,8 @@ import java.util.function.Predicate;
  */
 public class Leaderboard<N extends Number>
 {
+	public static final DecimalFormat DECIMAL_FORMAT = Util.make(new DecimalFormat("########0.00"), decimalFormat -> decimalFormat.setDecimalFormatSymbols(DecimalFormatSymbols.getInstance(Locale.ROOT)));
+
 	public static final Map<String, Leaderboard<?>> MAP = new LinkedHashMap<>();
 
 	public static <T extends Number> Leaderboard<T> add(String name, T defaultValue)
@@ -31,7 +37,7 @@ public class Leaderboard<N extends Number>
 
 		add("time_played", 0)
 				.value(stats -> stats.getValue(Stats.CUSTOM.get(Stats.PLAY_ONE_MINUTE)))
-				.string(IStatFormatter.TIME::format)
+				.formatTime()
 		;
 
 		add("deaths_per_hour", 0D)
@@ -40,7 +46,7 @@ public class Leaderboard<N extends Number>
 					int t = stats.getValue(Stats.CUSTOM.get(Stats.PLAY_ONE_MINUTE));
 					return d <= 0 || t < 72000 ? 0D : (double) d * 72000D / (double) t;
 				})
-				.string(IStatFormatter.DECIMAL_FORMAT::format)
+				.string(value -> DECIMAL_FORMAT.format(value.doubleValue()))
 		;
 
 		add("player_kills", 0)
@@ -53,7 +59,7 @@ public class Leaderboard<N extends Number>
 
 		add("damage_dealt", 0)
 				.value(stats -> stats.getValue(Stats.CUSTOM.get(Stats.DAMAGE_DEALT)))
-				.string(IStatFormatter.DIVIDE_BY_TEN::format)
+				.formatDivideByTen()
 		;
 
 		add("jumps", 0)
@@ -62,12 +68,12 @@ public class Leaderboard<N extends Number>
 
 		add("distance_walked", 0)
 				.value(stats -> stats.getValue(Stats.CUSTOM.get(Stats.WALK_ONE_CM)))
-				.string(IStatFormatter.DISTANCE::format)
+				.formatDistance()
 		;
 
 		add("time_since_death", 0)
 				.value(stats -> stats.getValue(Stats.CUSTOM.get(Stats.TIME_SINCE_DEATH)))
-				.string(IStatFormatter.TIME::format)
+				.formatTime()
 		;
 	}
 
@@ -83,7 +89,7 @@ public class Leaderboard<N extends Number>
 		defaultValue = def;
 		valueGetter = stats -> defaultValue;
 		filter = num -> !num.equals(defaultValue);
-		stringGetter = num -> IStatFormatter.DEFAULT.format(num.intValue());
+		stringGetter = num -> NumberFormat.getIntegerInstance(Locale.US).format(num.intValue());
 	}
 
 	public Leaderboard<N> value(Function<ServerStatisticsManager, N> v)
@@ -102,5 +108,53 @@ public class Leaderboard<N extends Number>
 	{
 		stringGetter = s;
 		return this;
+	}
+
+	public Leaderboard<N> formatDivideByTen()
+	{
+		return string(value -> DECIMAL_FORMAT.format(value.doubleValue() * 0.1D));
+	}
+
+	public Leaderboard<N> formatDistance()
+	{
+		return string(value -> {
+			double d0 = value.doubleValue() / 100.0D;
+			double d1 = d0 / 1000.0D;
+			if (d1 > 0.5D)
+			{
+				return DECIMAL_FORMAT.format(d1) + " km";
+			}
+			else
+			{
+				return d0 > 0.5D ? DECIMAL_FORMAT.format(d0) + " m" : value + " cm";
+			}
+		});
+	}
+
+	public Leaderboard<N> formatTime()
+	{
+		return string(value -> {
+			double d0 = value.doubleValue() / 20.0D;
+			double d1 = d0 / 60.0D;
+			double d2 = d1 / 60.0D;
+			double d3 = d2 / 24.0D;
+			double d4 = d3 / 365.0D;
+			if (d4 > 0.5D)
+			{
+				return DECIMAL_FORMAT.format(d4) + " y";
+			}
+			else if (d3 > 0.5D)
+			{
+				return DECIMAL_FORMAT.format(d3) + " d";
+			}
+			else if (d2 > 0.5D)
+			{
+				return DECIMAL_FORMAT.format(d2) + " h";
+			}
+			else
+			{
+				return d1 > 0.5D ? DECIMAL_FORMAT.format(d1) + " m" : d0 + " s";
+			}
+		});
 	}
 }
